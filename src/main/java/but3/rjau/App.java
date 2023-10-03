319ac987aeab324e504a64ccdbca7b9e878f5324
remi.jauzion <remi.jauzion@iut-rodez.fr>
2023-10-03 16:17:28 +0200

    test

---
+++ src/main/java/but3/rjau/App.java
@@ -0,0 +1,13 @@
+package but3.rjau;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/but3/rjau/AppTest.java
@@ -0,0 +1,14 @@
+package but3.rjau;
+
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+{
+    @Test
+    public void shouldAnswerWithTrue() { assertTrue(true); }
+}
